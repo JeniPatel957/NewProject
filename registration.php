f5218cbbee590f1f0a62f5838592edde1d0e8720
exam614 <SRIMCA\exam614@cs-u614.srimca.local>
2018-12-10 10:05:21 +0530

    registration commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Registration Page</h1>
+<form method='post'>
+Name : <input type='text' name='txtuname' required/><br/>
+Password : <input type='password' name='txtpass' required/><br/>
+Mobile No :<input type='number' name='txtmo' max='10' required/><br/>
+<input type='submit' name='btnsubmit' value='registration'/>
+</form>
+</body>
+</html>
